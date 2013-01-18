7bef35b209902e0916e21c0057afaf619fd2526b
Aravind M <arav.psgtech@gmail.com>
2013-01-18 13:57:29 +0530

    Revert "Aravind | #00 | Fixing build."
    This reverts commit 4b464de97a6afd2b2b05884445256bda3b5f6ce3.

--- src/main/java/db/migration/V1_3__ConfigurePermissions.java
+++ src/main/java/db/migration/V1_3__ConfigurePermissions.java
@@ -35,6 +35,7 @@ public class V1_3__ConfigurePermissions extends ReportsPermissions {
     }
 
     private void setExecuteOnlyPermissionOnSystemFolders(Roles role, List<Item> items) {
+        items.add(new Item(AccessRights.EXECUTE_ONLY.getPermissionMask(), new PermissionRecipient(role.name()), "repo:/Themes"));
         items.add(new Item(AccessRights.EXECUTE_ONLY.getPermissionMask(), new PermissionRecipient(role.name()), "repo:/System Properties"));
     }
 
